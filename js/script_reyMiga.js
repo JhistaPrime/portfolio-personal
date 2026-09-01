edc3360a34c8ed759918e510ef3ecad213ce05c4
JhistaPrime <christianbenjamin1@hotmail.com>
2026-09-01 02:15:31 -0300

    Add files via upload

--- js/script_reyMiga.js
+++ js/script_reyMiga.js
@@ -1,8 +1,37 @@
 const btn = document.getElementById('btn-submit');
+const nombre = document.getElementById('nombre');
+const email = document.getElementById('email');
+const mensaje = document.getElementById('mensaje');
+const parrafo = document.getElementById('warnings');
+
 
 document.getElementById('form')
  .addEventListener('submit', function(event) {
    event.preventDefault();
+
+   let warnings = '';
+   let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+   let hayError = false;
+   parrafo.innerHTML = '';
+
+   if(nombre.value.length < 3){
+      warnings += `El nombre no es valido <br>`;
+      hayError = true;
+   }
+   if(!regexEmail.test(email.value)){
+      warnings += `El email no es valido <br>`;
+      hayError = true;
+   }
+   if(mensaje.value == ''){
+      warnings += `El mensaje no puede estar vacío <br>`;
+      hayError = true;
+   }
+
+   if (hayError) {
+      parrafo.innerHTML = warnings;
+      return;
+   }
+
 
    btn.value = 'Enviando...';
 
@@ -12,9 +41,9 @@ document.getElementById('form')
    emailjs.sendForm(serviceID, templateID, this)
     .then(() => {
       btn.innerText = 'Enviar Mensaje';
-      alert('Sent!');
+      alert('¡Mensaje enviado!');
     }, (err) => {
-      btn.value = 'Send Email';
-      alert(JSON.stringify(err));
+      btn.value = 'Enviar Mensaje';
+      alert('No se pudo enviar el mensaje: ' +JSON.stringify(err));
     });
 });
